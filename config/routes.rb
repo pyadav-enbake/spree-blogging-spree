49c9c41ef45d9c349a4e02be6a1db71c5d401191
Sam Beam <sbeam@onsetcorps.net>
2012-01-11 15:54:55 -0500

    update routes for spree 1.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
-Rails.application.routes.draw do
-  match 'news/:year/:month/:day/:slug', :to => 'blog_entries#show', :as => :entry_permalink
-
-  match 'news/:year(/:month)(/:day)', :to => 'blog_entries#archive', :as => :news_archive,
-    :constraints => {:year => /(19|20)\d{2}/, :month => /[01]?\d/, :day => /[0-3]?\d/}
-
+Spree::Core::Engine.routes.append do
   resources :news, :controller => :blog_entries
 
   namespace :admin do
     resources :blog_entries
   end
+
+  match 'news/:year/:month/:day/:slug', :to => 'blog_entries#show', :as => :entry_permalink
+
+  match 'news/:year(/:month)(/:day)', :to => 'blog_entries#archive', :as => :news_archive,
+    :constraints => {:year => /(19|20)\d{2}/, :month => /[01]?\d/, :day => /[0-3]?\d/}
 
   match 'news/tag/:tag', :to => 'blog_entries#tag', :as => :tag
 end
